b0d601424ea538a066d2b4a18f28788b03e304a4
qq53 <1730806439@qq.com>
2014-03-20 20:00:53 +0800

    init

---
+++ src/calculator/cal.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import java.util.Stack;
+
+public class cal {
+	private Stack<String> oprd,optr;
+	private String ans;
+	private String[] var;
+	
+	public cal(){
+		oprd = new Stack<String>();
+		optr = new Stack<String>();
+		var = new String[100];
+		optr.push("#");
+	}
+	String process(String a){}
+	String get(){
+		;
+	};
+	char compare(String, String){}
+	String cal(String, String){}
+	String cal(String, String, String){}
+	Boolean save(const char[]){}
+	String load(const char[]){}
+	unsigned int getidx(string){}
+}
---
+++ src/calculator/console.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class console {
+	public static void main(String[] args) {
+		cal a = new cal();
+	}
+}
